c96ebc957e6abd808434775f5b47f7710faffbc1
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-04-28 12:05:41 +0530

    Learning printing numbers

---
+++ topic01_basics/C03_printNumberText.java
@@ -0,0 +1,8 @@
+package topic01_basics;
+
+public class C03_printNumberText {
+    public static void main(String[] args) {
+       System.out.println(3+1); 
+       System.out.println("3+1");
+    }
+}

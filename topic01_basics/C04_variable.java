e6fceeb420970ae4e6419e6c64b2e1b8b744bb53
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-04-29 00:17:33 +0530

    Learn variable

---
+++ topic01_basics/C04_variable.java
@@ -0,0 +1,10 @@
+package topic01_basics;
+
+public class C04_variable{
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x+7);
+        int y = 20;
+        System.out.println(y);
+      }
+}

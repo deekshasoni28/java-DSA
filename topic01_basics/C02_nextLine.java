057769bdc95e73108a4a9bcd8706d1d43c9ede47
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-04-28 11:59:51 +0530

    printing in next line

---
+++ topic01_basics/C02_nextLine.java
@@ -0,0 +1,9 @@
+package topic01_basics;
+
+public class C02_nextLine {
+    public static void main(String[] args) {
+        System.out.println("Deeksha");
+        System.out.println("Yogesh");
+        System.out.println("Subhi \nrishu");
+    }
+}

92c5780822e7aff40cf2f34c52770e155b31f390
Deeksha Soni <soni.deeksha2806@gmail.com>
2026-04-28 08:04:07 +0530

    Delete 1_basic directory

--- 1_basic/hello_deeksha.java
+++
@@ -1,8 +0,0 @@
-public class hello_deeksha {
-    public static void main(String[] args) {
-        System.out.println("Hello Deeksha...!");
-        System.out.print("Hello Rishu ...!");
-      // \n is used to print in new line
-        System.out.println("Hello Deeksha \nand Rishu...!");
-    }
-}

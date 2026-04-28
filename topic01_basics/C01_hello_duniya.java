79eaef544c3316022807481598cd180ea67165e8
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-04-28 11:33:55 +0530

    Hello duniya

--- topic01_basics/C01_hello_duniya.java
+++ topic01_basics/C01_hello_duniya.java
@@ -1,8 +1,7 @@
 package topic01_basics;
 
 public class C01_hello_duniya {
-    public static void main(String[] args) {
-       System.out.print("hello duniya");
-    }
-    
+        public static void main(String[] args) {
+            System.out.println("Hello duniya");
+        }
 }

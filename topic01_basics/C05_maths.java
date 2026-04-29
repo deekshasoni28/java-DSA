ad32d801fb3f9c34ad4b5b4e3d7c01503a10710a
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-04-29 07:36:13 +0530

    Maths in coding

---
+++ topic01_basics/C05_maths.java
@@ -0,0 +1,12 @@
+package topic01_basics;
+
+public class C05_maths {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 10;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}

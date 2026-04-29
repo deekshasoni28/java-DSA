b24e9da6035d82c68a8bbe1a0e593eca115eae63
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-04-29 07:34:56 +0530

    Volume of sphere

---
+++ topic01_basics/C07_volumeOfSphere.java
@@ -0,0 +1,8 @@
+package topic01_basics;
+public class C07_volumeOfSphere {
+    public static void main(String[] args) {
+        double radius = 6;
+        double volume = (4*3.14*3.14*3.14*radius)/3 ;
+        System.out.println("Volume of sphere = "+ volume);
+    }
+}

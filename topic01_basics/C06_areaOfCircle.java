a1e2380129c99d4ca44b6be7df6ae3e23801ea5d
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-04-29 07:35:37 +0530

    Area of circle

---
+++ topic01_basics/C06_areaOfCircle.java
@@ -0,0 +1,8 @@
+package topic01_basics;
+public class C06_areaOfCircle {
+    public static void main(String[] args) {
+        double radius = 6;
+        double area = 3.14*3.14*radius ;
+        System.out.println("Area of circle = "+ area);
+    }
+}

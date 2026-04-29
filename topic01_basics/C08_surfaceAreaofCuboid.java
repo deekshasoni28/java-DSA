dd7892c3e5a715245e729a0ec2d7ce703b055b0a
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-04-29 07:33:46 +0530

    total surface area of cuboid

---
+++ topic01_basics/C08_surfaceAreaofCuboid.java
@@ -0,0 +1,11 @@
+package topic01_basics;
+
+public class C08_surfaceAreaofCuboid{
+    public static void main(String[] args) {
+        int l = 2;
+        int h = 2;
+        int b = 2;
+        int area = 2*(l*b+b*h+l*h);
+        System.out.println("Surface area of cuboid : "+ area);
+    }
+}

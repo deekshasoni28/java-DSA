4ae1b2d93f26bff0a4b8b83f45fa933878c85f9b
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-05-13 19:44:54 +0530

    Data type

---
+++ topic01_basics/C14_dataTypeDivision.java
@@ -0,0 +1,10 @@
+package topic01_basics;
+
+public class C14_dataTypeDivision {
+    public static void main(String[] args) {
+        int a = 9 ;
+        int b = 4 ;
+        double c = a / b;
+        System.out.println(c);
+    }
+}

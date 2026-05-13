6c6e58b47f4180563464188da30c26e2cd5f58d7
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-05-13 19:56:09 +0530

    Increment decrement

---
+++ topic01_basics/C17_incre_Decre.java
@@ -0,0 +1,22 @@
+package topic01_basics;
+
+public class C17_incre_Decre {
+    public static void main(String[] args) {
+        int x = 10 ;
+       
+        System.out.println(++x);
+
+        int y = 6 ;
+        
+        System.out.println(y++);
+
+        int c = 1;
+        
+        System.out.println(c--);
+
+        int d = 10;
+        
+        System.out.println(--d);
+
+    }
+}

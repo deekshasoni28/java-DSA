7d74efc9f12aa9c99d1abc9d47f0ca4fd0a4de85
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-05-13 19:45:39 +0530

    Simple interest

---
+++ topic01_basics/C15_SI.java
@@ -0,0 +1,15 @@
+package topic01_basics;
+
+
+public class C15_SI {
+    public static void main(String[] args) {
+
+        int p = 100;
+        int r = 5 ;
+        int t = 8 ;
+
+        double si = (p * t * r)/100;
+
+        System.out.println(si);
+    }
+}

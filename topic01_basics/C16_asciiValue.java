43ecd332a9abc9950c679e5289c0551c1949822b
deekshasoni28 <soni.deeksha2806@gmail.com>
2026-05-13 19:56:57 +0530

    Ascii value

---
+++ topic01_basics/C16_asciiValue.java
@@ -0,0 +1,14 @@
+package topic01_basics;
+
+public class C16_asciiValue {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int x = ch;
+        System.out.println(x);
+
+
+        char c = 'a';
+        int y = (int)c;
+        System.out.println(y);
+    }
+}
